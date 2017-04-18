c0a7910ae9544c66032a8c31b1a397fd38aa4a17
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-04-18 14:28:26 +0100

    opening and plotting gc3.1 tile fracs

--- openAndConvert.py
+++ openAndConvert.py
@@ -14,94 +14,51 @@ from pdb import set_trace as browser
 from libs import git_info
 
 # Define paths and parameters
-dat_dir = "data/"
-exp_dir = "af398/"
-out_dir = "outputs/"
-fig_dir = "figs/"
+dat_file = 'data/qrparm.veg.frac'
+fig_file = "figs/GC3.1_vegFrac.png"
 
-pft_names = {101: 'BET-Tr', 102: 'BDT', 103: 'BET-Te',
-             201: 'NET',    202: 'NDT',
-             501: 'ESH',    502: 'DSH',
-               3: 'C3G',      4: 'C4G',
-             301: 'C3C',    401: 'C4C',
-             302: 'C3P',    402: 'C4P',
-               6: 'Ur' ,      7: 'Water', 8: 'Soil',   9: 'Ice'}
+pft_names = ['BL', 'NL', 'C3', 'C4', 'Shrub', 'Urban', 'Water', 'Soil', 'Ice']
 
-plot_order = [101, 102, 103, 201, 202, 501, 502, 3, 4, 301, 401, 302, 402, 6, 7, 8, 9]
-stash_contraint = iris.AttributeConstraint(STASH='m01s00i216')
-
+cmap = mpl_cm.get_cmap('brewer_YlGn_09')
+limits = [0.0, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5]
 ###############################################
 ## Load mask and remove coordinate system    ##
 ###############################################
-i = exp_dir
-dir = dat_dir + i
-
-map_points = [1, 2]
 
 git = 'repo: ' + git_info.url + '\n' + 'rev:  ' + git_info.rev
 
 crs_latlon = ccrs.PlateCarree()
 crs_proj   = ccrs.Robinson()
-cmap1 = mpl_cm.get_cmap('brewer_YlGn_09')
-cmap2 = mpl_cm.get_cmap('brewer_PRGn_11')
+
+px = 3
+py = 4
 
 def plot_map(fig, px, py, dat, pn, limits, cmap, title = 'yay', extend = 'max'):
-    norm = colours.BoundaryNorm(boundaries = limits, ncolors = 8)
+    norm = colours.BoundaryNorm(boundaries = limits, ncolors = len(limits)+1)
     
     ax = fig.add_subplot(px, py, pn, projection = crs_proj)
-    ax.set_extent((-180, 170, -65, 90.0), crs = crs_latlon)
+    #ax.set_extent((-180, 170, -65, 90.0), crs = crs_latlon)
     ax.coastlines(linewidth = 0.5, color = 'navy')
-    ax.gridlines(crs = crs_latlon, linestyle = '--')
     
     plt.gca().coastlines()
-    if (len(dat.shape) == 3 and dat.shape[0] == 1): dat = dat[0]
-    cbi = qplt.contourf(dat, limits, cmap = cmap, extend = extend, norm = norm) 
-    #qplt.contourf(dat) 
-    #plt.colorbar(cbi)
-    #titlei = title + labs[i][0]  
-    plt.title(title)
-
-def plot_fracs(cube, fig_out, sub_title, limits, cmap, *args):
-    figsize = (28, 12)
-    px = 5
-    py = 4
-    fig = plt.figure(figsize = figsize)
     
-    index  = pft_names.keys()
-    points = cube[0].coord('pseudo_level').points    
-    for x in range(0, len(pft_names)):
-        i  = index[x]
-        ii = np.where(points == i)[0]
-        plot_map(fig, px, py, cube[0][ii], x  + 1, limits, cmap, title = pft_names[i], *args)
+    cbi = iplt.contourf(dat, limits, cmap = cmap, extend = extend, norm = norm)
+    plt.title(title)   
+    return cbi
+    
+
+cube = iris.load_cube(dat_file)
+fig = plt.figure(figsize = (12* 1.5, 6 * 1.5))
+
+for x in range(0, cube.shape[0]):
+    cbi = plot_map(fig, px, py, cube[x], x + 1, limits, cmap, title = pft_names[x])
         
-    fig.suptitle(sub_title, fontsize = 16)
+fig.suptitle('GC3.1 tile frac', fontsize = 16)
 
-    fig.text(.6, .1, git)       
-    plt.savefig(fig_out, bbox_inches = 'tight')
+colorbar_axes = plt.gcf().add_axes([0.35, 0.25, 0.5, 0.08])
+colorbar = plt.colorbar(cbi, colorbar_axes, orientation='horizontal')
 
-p = 0
-test = False
-input_files = ['af398a.da19781201_00', 'af398a.da19931201_00']
-for input_file in input_files:
-    file_in = dir + input_file
-    file_out = out_dir + input_file + '.nc'
-    fig1_out = fig_dir + input_file + '.pdf'
-    fig2_out = fig_dir + input_file + 'diff' + '.pdf'
+fig.text(.4, .1, git)       
+plt.savefig(fig_file, bbox_inches = 'tight')
 
-    cube = iris.load(file_in, stash_contraint)
-   
-    p = p + 1
-    if (True in [p == i for i in map_points]):
-        plot_fracs(cube, fig1_out, input_file, limits = [0, 0.0001, 0.001, 0.01, 0.1, 0.2, 0.5, 1], cmap = cmap1)       
-        cube0 = cube
-        input_file0 = input_file
-                
-        if test:            
-            limits = [-0.1, -0.01, -0.001, -0.0001, 0.0001, 0.001, 0.01, 0.1]
-            sub_title = input_file + ' - ' + input_file0
-            plot_fracs(cube, fig2_out, sub_title, limits = limits, cmap = cmap2)            
-        
-        test = True
-       
-#yay = iris.load('/localscratch/wllf011/rjel/ae048.astart', stash_contraint)
-#iris.save(yay, 'yay.nc')
+
